e006f1fa2619bd8eb2a0c5253458a7f5b80425e4
Amilcar Pimentel <amilcaroswaldo10@gmail.com>
2021-02-22 23:24:32 -0600

    Creacion de modelos

---
+++ SistemaPasteleria/Models/ModelCatalogo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace SistemaPasteleria.Models
+{
+    class ModelCatalogo
+    {
+        public class Catalogo
+        {
+            public int id_catalogo { get; set; }
+            public string nombre { get; set; }
+
+        }
+        public class CatalogoContex : DbContext
+        {
+            public DbSet<Catalogo> Catalogos { get; set; }
+        }
+    }
+}
